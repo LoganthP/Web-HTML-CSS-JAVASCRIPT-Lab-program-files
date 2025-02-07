d08fa3a75d17385967490038f262358cc2128f0c
Loganth <170721512+LoganthP@users.noreply.github.com>
2025-02-07 16:30:20 +0530

    Delete HTML Lab/Part-B/Lab-5 - Copy.js

--- HTML Lab/Part-B/Lab-5 - Copy.js
+++
@@ -1,9 +0,0 @@
-const perimeter=(length,breadth)=>2*(length+breadth);
-
-const rectangleSpread=[10,5];
-const perimeterSpread=perimeter(...rectangleSpread);
-console.log(`perimeter Spread:${perimeterSpread}`);
-
-const rectangleRest=[8,6];
-const perimeterRest=perimeter(...rectangleRest);
-console.log(`perimeter Rest:${perimeterRest}`);
